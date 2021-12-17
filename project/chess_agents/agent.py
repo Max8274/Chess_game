a6a5194ab39570c2a5bc17f63d2dd3b92378d6cd
V-donck <tv26363@gmail.com>
2021-12-17 18:01:53 +0100

    4 layer minimax

--- project/chess_agents/agent.py
+++ project/chess_agents/agent.py
@@ -21,8 +21,66 @@ class Agent(ABC):
         self.name = "search agent chesser"
         self.author = "Maxime, Abe, Thijs"
 
-    def calculate_move(self, board: chess.Board):
 
+
+    def max_function(self, board: chess.Board,dept):
+        dept = dept -1
+
+        best_utility = -111110
+        best_move = None
+        for move in list(board.legal_moves):
+            #print("move "+ str(move))
+            #print("")
+            # Play the move
+            board.push(move)
+            if dept <= 0:
+                worst_utility = self.utility.board_value(board)
+            else:
+                #print(dept)
+                worst_utility,worst_move = self.min_function(board,dept)
+            if worst_utility > best_utility:
+                #print("move beter")
+                #print(str(move) +"worsts "+ str(worst_utility) + "best " +str(best_utility))
+                best_move = move
+                best_utility = worst_utility
+            board.pop()
+        if best_move == None:
+            best_utility = 100000000
+        return best_utility,best_move
+
+
+
+    def min_function(self, board: chess.Board, dept):
+        dept = dept -1
+        worst_utility = 100000000000
+        worst_move = None
+        for move in list(board.legal_moves):
+
+            board.push(move)
+            if dept <= 0:
+                value = self.utility.board_value(board)
+            else:
+                #print(dept)
+                value,best_move = self.max_function(board,dept)
+
+           # print("move2"+ str(move) +"   value   "+ str(value))
+            # If this is better than all other previous moves, store this move and its utility
+            if value < worst_utility:
+                #print("move2 slechter!!!!"+ str(value))
+                worst_move = move
+                worst_utility = value
+            board.pop()
+        if worst_move == None:
+            worst_utility = -100000000
+        return worst_utility, worst_move
+
+    def calculate_move(self, board: chess.Board):
+        if board.turn == chess.WHITE:
+            value,move = self.max_function(board,4)
+        else:
+            value, move = self.min_function(board,3)
+        return move
+        """
         start_time = time.time()
 
         # If the agent is playing as black, the utility values are flipped (negative-positive)
@@ -62,4 +120,6 @@ class Agent(ABC):
 
             # Revert the board to its original state
             board.pop()
+            
         return best_move
+"""
--- project/chess_utilities/utility.py
+++ project/chess_utilities/utility.py
@@ -13,7 +13,7 @@ class Utility(ABC):
         n_white += len(board.pieces(piece_type=chess.KNIGHT, color=chess.WHITE))*3
         n_white += len(board.pieces(piece_type=chess.ROOK, color=chess.WHITE))*5
         n_white += len(board.pieces(piece_type=chess.QUEEN, color=chess.WHITE))*10
-        n_white += len(board.pieces(piece_type=chess.KING, color=chess.WHITE))*100
+        n_white += len(board.pieces(piece_type=chess.KING, color=chess.WHITE))*1000
 
         n_black = 0
         n_black += len(board.pieces(piece_type=chess.PAWN, color=chess.BLACK))*1
@@ -21,5 +21,5 @@ class Utility(ABC):
         n_black += len(board.pieces(piece_type=chess.KNIGHT, color=chess.BLACK))*3
         n_black += len(board.pieces(piece_type=chess.ROOK, color=chess.BLACK))*5
         n_black += len(board.pieces(piece_type=chess.QUEEN, color=chess.BLACK))*10
-        n_white += len(board.pieces(piece_type=chess.KING, color=chess.BLACK))*100
+        n_black += len(board.pieces(piece_type=chess.KING, color=chess.BLACK))*1000
         return n_white - n_black
